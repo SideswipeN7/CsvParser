6035a938122a070e0d27d3c57612f52e27c4bfbd
SideswipeN7 <pawel.wanot@gmail.com>
2021-07-20 18:50:08 +0200

    New types including nullable

---
+++ Sources/CsvParser/CsvParser/Resolvers/NullableTypeValueResolver.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CsvParser.Resolvers
+{
+    internal static class NullableTypeValueResolver
+    {
+        public static object? GetValue(Type type, string value)
+        {
+            return type.FullName switch
+            {
+                string name when name.Contains(nameof(Int16)) => ParseNullableInt16(value),
+                string name when name.Contains(nameof(Int32)) => ParseNullableInt32(value),
+                string name when name.Contains(nameof(Int64)) => ParseNullableInt64(value),
+                string name when name.Contains(nameof(Decimal)) => ParseNullableDecimal(value),
+                string name when name.Contains(nameof(Double)) => ParseNullableDouble(value),
+                string name when name.Contains(nameof(Boolean)) => ParseNullableBool(value),
+                string name when name.Contains(nameof(Byte)) => ParseNullableByte(value),
+                string name when name.Contains(nameof(Guid)) => ParseNullableGuid(value),
+                _ => value,
+            };
+        }
+
+        private static short? ParseNullableInt16(string value)
+        {
+            var isValid = short.TryParse(value, out short parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static int? ParseNullableInt32(string value)
+        {
+            var isValid = int.TryParse(value, out int parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static long? ParseNullableInt64(string value)
+        {
+            var isValid = long.TryParse(value, out long parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static decimal? ParseNullableDecimal(string value)
+        {
+            var isValid = decimal.TryParse(value, out decimal parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static double? ParseNullableDouble(string value)
+        {
+            var isValid = double.TryParse(value, out double parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static bool? ParseNullableBool(string value)
+        {
+            var isValid = bool.TryParse(value, out bool parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static byte? ParseNullableByte(string value)
+        {
+            var isValid = byte.TryParse(value, out byte parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+
+        private static Guid? ParseNullableGuid(string value)
+        {
+            var isValid = Guid.TryParse(value, out Guid parsedValue);
+
+            return isValid ? parsedValue : null;
+        }
+    }
+}
--- Sources/CsvParser/CsvParser/Resolvers/TypeValueResolver.cs
+++ Sources/CsvParser/CsvParser/Resolvers/TypeValueResolver.cs
@@ -5,17 +5,19 @@ namespace CsvParser.Resolvers
 {
     internal static class TypeValueResolver
     {
-        public static object GetValue(Type type, string value)
+        public static object? GetValue(Type type, string value)
         {
             return type.Name switch
             {
-                nameof(Int32) => int.Parse(value),
                 nameof(Int16) => short.Parse(value),
+                nameof(Int32) => int.Parse(value),
                 nameof(Int64) => long.Parse(value),
                 nameof(Decimal) => decimal.Parse(value.Replace(',', '.'), CultureInfo.InvariantCulture),
                 nameof(Double) => double.Parse(value.Replace(',', '.'), CultureInfo.InvariantCulture),
                 nameof(Boolean) => bool.Parse(value),
-                nameof(String) => value,
+                nameof(Byte) => byte.Parse(value),
+                nameof(Guid) => Guid.Parse(value),
+                nameof(Nullable) => NullableTypeValueResolver.GetValue(type, value),
                 _ => value,
             };
         }
